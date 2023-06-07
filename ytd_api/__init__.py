70bcd57a8a0c0ee542e79748252ec41c9f173375
SL-Pirate <isiraherath626@gmail.com>
2023-06-07 11:11:53 +0530

    download via api end point to api module

--- ytd_api/__init__.py
+++ ytd_api/__init__.py
@@ -3,5 +3,6 @@ from flask import Blueprint
 from ytd_api.audio_api import audio_api
 from ytd_api.video_api import vid_api
 from ytd_api.qual_api import qual_api
+from ytd_api.get_multimedia import get_multimedia_via_api
 
-endpoints: list[Blueprint] = [audio_api, vid_api, qual_api]
+endpoints: list[Blueprint] = [audio_api, vid_api, qual_api, get_multimedia_via_api]
--- ytd_web/get_mulimedia.py
+++ ytd_web/get_mulimedia.py
@@ -26,14 +26,3 @@ def file_get_aud():
             return render_template("went_wrong.html", exception=e, links=StaticLinks)
     else:
         return redirect("/home")
-
-@get_miltimedia_page.route("/download/path", methods=['GET'])
-def download_from_link():
-    protected_files = ["secrets.key"]
-    for protected_file in protected_files:
-        if protected_file in request.args.get('file_name'):
-            return {'status': 403, 'description': "Illegal file name!"}, 403
-    try:
-        return send_file(request.args.get('file_name'), as_attachment=True)
-    except FileNotFoundError:
-        return {'status': 404, 'description': "link expired!"}, 404
