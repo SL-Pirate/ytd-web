f1a9f744ffbe5a20480aca2c1828ebdedcc190e6
Isira Herath <isiraherath626@gmail.com>
2023-10-30 01:55:35 +0530

    improved API authentication by setting a TTL

--- server/ytd_web_api/auth.py
+++ server/ytd_web_api/auth.py
@@ -1,13 +1,17 @@
 from configparser import ConfigParser
 import hashlib
+import time
 
 _config = ConfigParser()
 _config.read(".env")
 _api_key = _config["session"]["key"]
     
 def authenticate(request) -> bool:
-    if request.META.get('HTTP_TOKEN') == hashlib.md5(
-        f"{request.META.get('HTTP_TIMESTAMP')}{_api_key}".encode()
+    timestamp = request.META.get('HTTP_TIMESTAMP')
+    # checking timestamp is within 5 minutes
+    # and token is valid
+    if not timestamp < time.time * 1000 - 300000 and request.META.get('HTTP_TOKEN') == hashlib.md5(
+        f"{timestamp}{_api_key}".encode()
     ).hexdigest():
         return True
     else:
