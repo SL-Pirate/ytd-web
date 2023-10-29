e8c49b067a58fbeccf8d3c8d55749afb0ad329ee
Isira Herath <isiraherath626@gmail.com>
2023-10-30 01:55:35 +0530

    improved API authentication by setting a TTL

--- server/ytd_web_core/urls.py
+++ server/ytd_web_core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from ytd_web_core.views import serve_downloadable
 
 urlpatterns = [
-    path('downloads/<int:id>/', serve_downloadable)
+    path('downloads/<int:id>/', serve_downloadable),
+    path('downloads/<int:id>', serve_downloadable)
 ]
